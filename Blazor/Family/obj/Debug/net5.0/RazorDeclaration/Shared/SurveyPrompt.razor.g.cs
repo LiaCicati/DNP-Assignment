c0cf4d4c3324ee4605fac9ae4298551ad9a78ce9
Lori <loredanacicati@gmail.com>
2021-10-02 20:54:58 +0200

    Styled Login component

--- Blazor/Family/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ Blazor/Family/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace Family.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 1 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 2 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 3 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 4 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 5 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 6 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 7 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 8 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 9 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Family;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
+#line 10 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\_Imports.razor"
 using Family.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Family.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Lia Cicati\DNP-Assignment1\Blazor\Family\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\Loredana Cicati\DNP-Assignment1\Blazor\Family\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
